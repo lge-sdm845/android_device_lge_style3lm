1cc0353340e1a68561341a295147717fcc55e9c0
Han Sol Jin <hansol@hansol.ca>
2024-04-15 11:21:52 -0700

    judypn: use multi-device extract-utils
    Change-Id: Ieb4b00bbe3eaf4fa54fe15d222f84c2c76464442

--- extract-files.sh
+++ extract-files.sh
@@ -6,64 +6,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
-DEVICE=judypn
-VENDOR=lge
+export DEVICE=judypn
+export DEVICE_COMMON=sdm845-common
+export VENDOR=lge
+export VENDOR_COMMON=${VENDOR}
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-source "$HELPER"
-
-# Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
-
-ONLY_COMMON=
-ONLY_TARGET=
-KANG=
-SECTION=
-
-while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        --only-common )
-                ONLY_COMMON=true
-                ;;
-        --only-target )
-                ONLY_TARGET=true
-                ;;
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
-    esac
-    shift
-done
-
-if [ -z "$SRC" ]; then
-    SRC=adb
-fi
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-extract "${MY_DIR}"/proprietary-files.txt "${SRC}" "${SECTION}"
-
-"${MY_DIR}"/setup-makefiles.sh
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,29 +8,9 @@
 
 set -e
 
-DEVICE=judypn
-VENDOR=lge
+export DEVICE=judypn
+export DEVICE_COMMON=sdm845-common
+export VENDOR=lge
+export VENDOR_COMMON=${VENDOR}
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
-
-# Copyright headers and guards
-write_headers
-
-write_makefiles "${MY_DIR}"/proprietary-files.txt true
-
-# Finish
-write_footers
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
