a171cbbda5ff4699336e2b7e1e1d84051b6c2316
Han Sol Jin <hansol@hansol.ca>
2025-02-26 03:02:56 -0800

    style3lm: switch to py extract-utils
    Change-Id: I2c59c02bffb1115d9dd952d6f39b7c231d19ad2d

---
+++ extract-files.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    "vendor/lge/sdm845-common",
+    "vendor/qcom/opensource/display"
+]
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/lib/liblgdnnsnpe.so',
+        'vendor/lib/libSNPE.so'
+    ): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so')
+}  # fmt: skip
+
+
+module = ExtractUtilsModule(
+    'style3lm',
+    'lge',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sdm845-common', module.vendor)
+    utils.run()
