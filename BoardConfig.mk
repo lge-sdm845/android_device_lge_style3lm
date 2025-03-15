3eeeb77948268fe1da8f5b181c63d13b70ef997d
Han Sol Jin <hansol@hansol.ca>
2025-03-15 10:42:30 -0700

    style3lm: inherit dynamic BoardConfig
    Change-Id: I3c995e1124c253353729d6bca3fb6569df950f43

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ DEVICE_PATH := device/lge/style3lm
 BOARD_SUPER_PARTITION_SIZE := 17179869184
 
 # Inherit from common device tree
-include device/lge/sdm845-common/BoardConfigCommon.mk
+include device/lge/sdm845-common/BoardConfigDynamic.mk
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
