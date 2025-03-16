9bac0e024869b9e7efc4c37006d8daabe458ae8d
Han Sol Jin <hansol@hansol.ca>
2025-03-16 13:29:29 -0700

    style3lm: move TARGET_*_PROP to BoardConfig
    Change-Id: I2cd9ef430aac8a006c0e2c7ec4af1bea65a5bb33

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,5 +32,9 @@ $(call soong_config_set,LGE_LIGHTS_HAL,INCLUDE_DIR,$(DEVICE_PATH)/include)
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 17179869184
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # inherit from the proprietary version
 include vendor/lge/style3lm/BoardConfigVendor.mk
